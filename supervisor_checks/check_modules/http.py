48cc7072fa61707d6188401ed6f757f3ff7a1da6
Jason Lee <jason.lee@alation.com>
2018-07-30 17:48:33 -0700

    minor cleanup

--- supervisor_checks/check_modules/http.py
+++ supervisor_checks/check_modules/http.py
@@ -79,8 +79,9 @@ class HTTPCheck(base.BaseCheck):
             headers['Authorization'] = 'Basic %s' % base64.b64encode(
                 auth_str.encode()).decode()
 
-        if self._config.get('headers'):
-            headers.update(self._config.get('headers'))
+        config_headers = self._config.get('headers')
+        if config_headers:
+            headers.update(config_headers)
             # auto apply content type if json argument is passed in
             if self._config.get('json'):
                 headers['Content-Type'] = 'application/json'
@@ -89,7 +90,7 @@ class HTTPCheck(base.BaseCheck):
         if self._config.get('json'):
             body = json.dumps(self._config.get('json'))
 
-        connection.request(self._config.get('method'), self._config.get('url'), body, 
+        connection.request(self._config.get('method'), self._config['url'], body, 
             headers=headers)
 
         return connection.getresponse()
